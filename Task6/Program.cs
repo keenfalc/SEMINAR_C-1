9b916ced08f9fca378d146687e4e1a7b898cd695
IvanSokolov <keenfalcon@mail.ru>
2022-05-01 01:38:48 +0300

    Написали программу 7 ДЗ

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.Write(a);
+}
+else if (b > a && b > c)
+{
+    Console.Write(b);
+}
+else if (c > a && c > b)
+{
+    Console.Write(c);
+}
