2080e43a5e14b03175ac4d83dca425a094a0a7a7
IvanSokolov <keenfalcon@mail.ru>
2022-05-01 01:38:04 +0300

    Написали программу 6 ДЗ

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+{
+    if(a > b) 
+    Console.Write(a);
+    if(b > a)
+    Console.Write(b);   
+}
