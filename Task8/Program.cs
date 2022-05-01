d2ade06115113f325e2b44910a72824e49296ea6
IvanSokolov <keenfalcon@mail.ru>
2022-05-01 11:07:04 +0300

    Доработал богоподобный код, который оставил мне 3 часа сна

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,13 +1,15 @@
 ﻿
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int temp = number - 1;
 for (int i = 1; i <= number; i++)
 {
     if (i % 2 == 0)
-    {
-        
+    {    
         Console.Write(i);
-        if (i != number)
-        Console.Write(" , ");
+        if (i < temp)
+        {
+             Console.Write(" , ");   
+        }   
     }
 }
