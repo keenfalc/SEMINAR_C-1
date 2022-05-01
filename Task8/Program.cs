5cef19fd27c49e7706bbe381080c59e4d152e8d9
IvanSokolov <keenfalcon@mail.ru>
2022-05-01 09:38:31 +0300

    Внес исправление в Task8 ДЗ

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -5,6 +5,9 @@ for (int i = 1; i <= number; i++)
 {
     if (i % 2 == 0)
     {
-        Console.Write(i + " , ");
+        
+        Console.Write(i);
+        if (i != number)
+        Console.Write(" , ");
     }
 }
