5f999ddf3134828dfa4c5744df30cff403ee1de2
IvanSokolov <keenfalcon@mail.ru>
2022-05-11 20:21:21 +0300

    Написали программу 9

---
+++ Task9/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+На вход принимается трехзначное число, на выход показывает его последнюю цифру. */
+
+Console.WriteLine("Введите значение числа n: ");
+string text = Console.ReadLine();
+int y = text[text.Length - 1] - '0';
+Console.WriteLine("Последняя цифра числа {0,5}", y);
